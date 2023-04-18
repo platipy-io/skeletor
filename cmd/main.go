4736a43c7c900025f9f91c32c989a228422d64ca
Maxime Vidori <maxime.vidori@gmail.com>
2023-04-18 21:37:29 +0200

    Add some logs to the golang binary

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s: 200", r.URL.Path)
 		fmt.Fprintf(w, "Hello, World!")
 	})
 
